66ac2cb6442802dbfdb9ef27eadf27ad504599f2
anvesh987987 <anvesh.sodisetti@gmail.com>
2025-09-21 20:10:49 +0530

    handling Multiple handlers with different scenarios with array binding and seperated with comma's and also wotk with next() func

--- src/app.js
+++ src/app.js
@@ -1,33 +1,96 @@
 const express = require("express")
 const app = express()
 
-app.get("/spouse",(req,res) => {
+
+
+/* Here in this we handling the multiple routes with next() by calling the routes 
+until server responseds with response */
+app.use("/user", (req, res, next) => {
+    console.log("Handling the route user with 1st response")
+    next()
+
+}, (req, res) => {
+    res.send("2nd reesponse for user")
+    console.log("Handling the route user with 2nd response")
+}, (req, res) => {
+    res.send("3rd reesponse for user")
+    console.log("Handling the route user with 3rd response")
+})
+
+/*  Here another type we handling the multiple routes with next() by calling the routes in array 
+until server responseds with response */
+app.use("/testUser",
+    [(req, res, next) => {
+        console.log("Handling the route testUser with 1st response")
+        next()
+    }, (req, res, next) => {
+        // res.send("2nd reesponse for testUser")
+        console.log("Handling the route testUser with 2nd response")
+        next()
+    }, (req, res) => {
+        res.send("3rd reesponse for testUser")
+        console.log("Handling the route testUser with 3rd response")
+    }, (req, res) => {
+        res.send("4th reesponse for testUser")
+        console.log("Handling the route testUser with 4tgh response")
+    }, (req, res) => {
+        res.send("5th reesponse for testUser")
+        console.log("Handling the route testUser with 5th response")
+    }])
+
+
+/*  Here another type we handling the multiple routes with next() by calling
+ the some routes in array and some are like objects like taditional method
+until server responseds with response */
+
+// app.use("/route,rH,[rH2,rH3],rH4,rH5")
+
+app.use("/testUser2",
+    (req, res, next) => {
+        console.log("Handling the route testUser2 with 1st response")
+        next()
+    }, [(req, res, next) => {
+        // res.send("2nd reesponse for testUser2")
+        console.log("Handling the route testUser2 with 2nd response")
+        next()
+    }, (req, res) => {
+        res.send("3rd reesponse for testUser2")
+        console.log("Handling the route testUser2 with 3rd response")
+    }], (req, res) => {
+        res.send("4th reesponse for testUser2")
+        console.log("Handling the route testUser2 with 4tgh response")
+    }, (req, res) => {
+        res.send("5th reesponse for testUser2")
+        console.log("Handling the route testUser2 with 5th response")
+    })
+
+app.get("/spouse", (req, res) => {
     res.send("Hello Rani from server")
 })
 
-app.get("/",(req,res) => {
+app.get("/", (req, res) => {
     res.send("Hello Rani and Anvesh from server")
 })
 
 // Dynamic routes able to give the user name and password
-app.get("/user/:userId/:name/:password",(req,res) => {
+app.get("/user/:userId/:name/:password", (req, res) => {
     console.log(req.params)
-    res.send({firstName:"Anvesh",lastName:"Sodisetti"})
+    res.send({ firstName: "Anvesh", lastName: "Sodisetti" })
 })
 
 //
-app.post("/user",(req,res) => {
+app.post("/user", (req, res) => {
     res.send("Hello posted successfully")
 })
 
-app.delete("/user",(req,res) => {
+app.delete("/user", (req, res) => {
     res.send("Hello Deleted successfully")
 })
 
-app.get("/bride",(req,res) => {
+app.get("/bride", (req, res) => {
     res.send("Hello anvesh from server")
 })
 
-app.listen(3000,() => {
+app.listen(3000, () => {
     console.log("Server runnning smoothly....")
 })
