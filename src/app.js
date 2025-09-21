8a20709f0d8c8306da7417c403f406ea4a1a8623
anvesh987987 <anvesh.sodisetti@gmail.com>
2025-09-21 18:38:02 +0530

    checking the HTTP Calls

--- src/app.js
+++ src/app.js
@@ -1,10 +1,26 @@
 const express = require("express")
 const app = express()
 
-app.use("/hello",(req,res) => {
-    res.send(" Hello server")
+app.get("/spouse",(req,res) => {
+    res.send("Hello Rani from server")
+})
+
+app.get("/",(req,res) => {
+    res.send("Hello Rani and Anvesh from server")
+})
+
+app.post("/user",(req,res) => {
+    res.send("Hello posted successfully")
+})
+
+app.delete("/user",(req,res) => {
+    res.send("Hello Deleted successfully")
+})
+
+app.get("/bride",(req,res) => {
+    res.send("Hello anvesh from server")
 })
 
 app.listen(3000,() => {
-    console.log("sucessfully server listening")
+    console.log("Server runnning smoothly....")
 })
